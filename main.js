6c4920a27c40f5579216fc8ba892e267748d5a4e
keside <wilsonezeala@yahoo.com>
2022-03-08 23:48:53 +0100

    feat: theme switching

--- main.js
+++ main.js
@@ -1,6 +1,24 @@
 const menuBtn = document.getElementById('menu-btn');
+const body = document.querySelector('body');
+const themeToggle = document.querySelectorAll('.theme-toggle');
+const toggles = document.querySelectorAll('.toggle');
 
-console.log(menuBtn);
-menuBtn.addEventListener('click', () => {
- console.log('er');
-});
+function toggleTheme() {
+    body.classList.toggle('theme__dark');
+}
+
+function flipThemeSwitch() {
+    themeToggle.forEach((toggle) => {
+        toggle.addEventListener('click', (e) => {
+            toggleTheme();
+            toggles.forEach((toggle) => {
+                toggle.classList.remove('active');
+            });
+            if (e.target.classList.contains('toggle')) {
+                e.target.classList.add('active');
+            }
+        });
+    });
+}
+
+flipThemeSwitch();
