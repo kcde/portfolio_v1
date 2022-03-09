19a5d1830bdebb604215779c1eb1dc2f6ef81b32
keside <wilsonezeala@yahoo.com>
2022-03-09 10:17:03 +0100

    feat:  detect users prefered theme

--- main.js
+++ main.js
@@ -5,6 +5,33 @@ const toggles = document.querySelectorAll('.toggle');
 
 function toggleTheme() {
     body.classList.toggle('theme__dark');
+    themeToggleSwitchCordinator();
+}
+
+function saveThemeSelection(mode = 'default') {
+    localStorage.setItem('kcde_portfolio_theme', mode);
+}
+
+function themeToggleSwitchCordinator() {
+    const lightToggles = document.querySelectorAll('.light-toggle');
+    const darkToggles = document.querySelectorAll('.dark-toggle');
+    if (body.classList.contains('theme__dark')) {
+        lightToggles.forEach((toggle) => {
+            toggle.classList.remove('active');
+        });
+
+        darkToggles.forEach((toggle) => {
+            toggle.classList.add('active');
+        });
+    } else {
+        darkToggles.forEach((toggle) => {
+            toggle.classList.remove('active');
+        });
+
+        lightToggles.forEach((toggle) => {
+            toggle.classList.add('active');
+        });
+    }
 }
 
 function flipThemeSwitch() {
@@ -15,13 +42,41 @@ function flipThemeSwitch() {
                 !e.target.classList.contains('active')
             ) {
                 toggleTheme();
-                toggles.forEach((toggle) => {
-                    toggle.classList.remove('active');
-                });
-                e.target.classList.add('active');
+                if (e.target.classList.contains('dark-toggle')) {
+                    saveThemeSelection('dark');
+                } else {
+                    saveThemeSelection();
+                }
             }
         });
     });
+    themeToggleSwitchCordinator();
 }
 
-flipThemeSwitch();
+function setTheme() {
+    const userThemeSelection = localStorage.getItem('kcde_portfolio_theme');
+    if (userThemeSelection) {
+        switch (userThemeSelection) {
+            case 'dark':
+                body.classList.add('theme__dark');
+                break;
+            default:
+                body.classList.remove('theme__dark');
+        }
+
+        flipThemeSwitch();
+    } else {
+        const userPrefersDarkMode = window.matchMedia(
+            '(prefers-color-scheme: dark)'
+        ).matches;
+
+        if (userPrefersDarkMode) {
+            body.classList.add('theme__dark');
+        } else {
+            body.classList.remove('theme__dark');
+        }
+        flipThemeSwitch();
+    }
+}
+
+setTheme();
